899b3e25fe083e0163a35b972eb5c9bc358d0df7
Tanish-ghai <tghai264@gmail.com>
2025-09-23 14:00:46 +0530

    Add files via upload

---
+++ Experiment-3.1/script.js
@@ -0,0 +1,11 @@
+const themeBtn = document.getElementById("theme-btn");
+const body = document.body;
+
+themeBtn.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    themeBtn.textContent = "☀️ Light Mode";
+  } else {
+    themeBtn.textContent = "🌙 Dark Mode";
+  }
+});
